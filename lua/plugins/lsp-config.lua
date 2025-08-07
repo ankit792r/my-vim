f219e73e29d7ce85bdd08f1d33e5b1180f8fdae8
Ankit Prajapati <ap8424856@gmail.com>
2025-08-07 22:32:01 +0530

    added git

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,6 +20,10 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
+      lspconfig.pyright.setup({
+        capabilities = capabilities
+      })
+
       lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
